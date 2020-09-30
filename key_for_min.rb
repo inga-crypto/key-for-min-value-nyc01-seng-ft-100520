ca2c544ca981f5c8f399c00e006e70a9f37af1ca
Inga <ijaanimets@gmail.com>
2020-09-30 19:19:09 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,9 +5,9 @@ def key_for_min_value(name_hash)
     min_value = nil
     min_key = nil
   name_hash.each do |key, value|
-    if value < min_value
-      
-      min_key = min_value
+    if min_value == nil || value < min_value
+      min_key = key
+      min_value = value
     end
     
   end
